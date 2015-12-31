2d502f7e8ac5212e7a78b60d8a953018c25fa648
C.K <3517373@qq.com>
2015-12-31 22:14:09 +0800

    1

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 import tushare as ts
 def run():
     df = ts.get_today_all()
-    df.to_csv('d:\python\profit.csv',encoding='utf-8')
+    df.to_csv('./data/profit.csv',encoding='utf-8')
 
 if __name__ =='__main__':
     run()
